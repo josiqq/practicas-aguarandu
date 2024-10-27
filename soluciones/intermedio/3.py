45c0af16e27843bb04c3c4cf223964a513a6fabb
josiqq <josiwasd@gmail.com>
2024-10-27 00:03:18 -0300

    resolviendo intermedios

--- soluciones/intermedio/3.py
+++ soluciones/intermedio/3.py
@@ -1,2 +1,32 @@
 # https://github.com/josiqq/practicas-aguarandu/blob/main/enunciados/intermedio/3.md
 
+def merge_sort(lista):
+    if len(lista) <= 1:
+        return lista
+    
+    medio = len(lista) // 2
+    izquierda = lista[:medio]
+    derecha = lista[medio:]
+
+    izquierda = merge_sort(izquierda)
+    derecha = merge_sort(derecha)
+
+    return merge(izquierda, derecha)
+
+def merge(izquierda, derecha):
+    resultado = []
+    i = j = 0
+
+    while i < len(izquierda) and j < len(derecha):
+        if izquierda[i] < derecha[j]:
+            resultado.append(izquierda[i])
+            i += 1
+        else: 
+            resultado.append(derecha[j])
+            j += 1
+    resultado.extend(izquierda[i:])
+    resultado.extend(derecha[j:])
+    return resultado
+
+print(f"Entrada: {[38, 27, 43, 3, 9, 82, 10]}")
+print(f"Salida: {merge_sort([38, 27, 43, 3, 9, 82, 10])}")
--- test.py
+++ test.py
@@ -1,4 +1,34 @@
-es_primo = [True] * (10+1)
-es_primo[0], es_primo[1] = False, False
+def merge_sort(lista):
+    if len(lista) <= 1:
+        return lista
+    
+    medio = len(lista) // 2
+    izquierda = lista[:medio]
+    derecha = lista[medio:]
 
-print(es_primo)
+    izquierda = merge_sort(izquierda)
+    derecha = merge_sort(derecha)
+
+    print(f"Izquierda: {izquierda}")
+    print(f"Derecha: {derecha}")
+
+def merge(izquierda, derecha):
+    resultado = []
+    i = j = 0
+
+    # Compara los elementos y los agrega en orden al resultado
+    while i < len(izquierda) and j < len(derecha):
+        if izquierda[i] < derecha[j]:
+            resultado.append(izquierda[i])
+            i += 1
+        else:
+            resultado.append(derecha[j])
+            j += 1
+
+    # Agrega los elementos restantes de ambas mitades (si quedan)
+    resultado.extend(izquierda[i:])
+    resultado.extend(derecha[j:])
+
+    return resultado
+
+print(merge_sort([1, 2]))
